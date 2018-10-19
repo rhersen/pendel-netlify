7e5ab6a59312bb22eb600c2e84f28687d8513638
sw-yx <swyx@swyx.io>
2018-10-18 21:07:50 -0400

    create-react-app

--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -1,13 +1,7 @@
-// show object spread works, i.e. babel works
-const obj = {
-  foo: 'bar'
-};
 export function handler(event, context, callback) {
-  console.log('qsp', event.queryStringParameters);
-  console.log('**********');
-  console.log('mvsp', event.multiValueQueryStringParameters);
+  console.log("queryStringParameters", event.queryStringParameters)
   callback(null, {
     statusCode: 200,
-    body: JSON.stringify({ msg: 'Hello, World!', ...obj })
-  });
+    body: JSON.stringify({ msg: "Hello, World!" })
+  })
 }
